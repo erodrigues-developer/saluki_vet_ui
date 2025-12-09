e666bac223169b8c9108b506d0807df62a61b261
erodrigues-developer <erodrigues.developer@gmail.com>
2025-12-09 01:02:49 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['nuxtjs-naive-ui'],
+  css: ['vfonts/Lato.css', 'vfonts/FiraCode.css'],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar',
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+  },
+})
